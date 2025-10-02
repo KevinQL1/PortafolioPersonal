c2c80b417fcc762010500c2d54ecfca726404de1
23-06-96 <kevin.quintero@konecta.com>
2025-10-02 12:16:28 -0500

    solution tailwindcss

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  devIndicators: {
-    buildActivity: false,
-  },
-}
+import type { NextConfig } from "next";
 
-module.exports = nextConfig
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+};
+
+export default nextConfig;
